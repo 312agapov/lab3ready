b932c8bd9468e11e3701ce9f5736cd34dd133a0f
312agapov <62393050+312agapov@users.noreply.github.com>
2020-04-21 16:41:08 +0300

    Update programmist.cpp

--- programmist.cpp
+++ programmist.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-programmist::programmist(string name) : sotrudnik()
+programmist::programmist(string name) : sotrudnik() //конструктор с параметрами
 {
     sname=name;
 }
 
-programmist::programmist()
+programmist::programmist()   //конструктор без параметров
 {
     sname = "da,zakazival";
 }
 
-programmist::programmist(const programmist &obj)
+programmist::programmist(const programmist &obj) //конструктор копирования
 {
     sname = obj.sname;
 }
